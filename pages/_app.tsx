401bda1ae08e247be1d21254b6b264903da4dc0d
Danna <daeunlee0720@gmail.com>
2022-05-08 16:32:25 +0900

    dev: 메타데이터 입력

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,20 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>HACKER</title>
         <link rel="shortcut icon" href="/favicon.png" />
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+        />
+        <meta name="description" content="콩 심은 데 콩 나고, 커밋한 데 머리 난다!" />
+        <meta name="keywords" content="콩 심은 데 콩 나고, 커밋한 데 머리 난다!" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="HACKER" />
+        <meta property="og:url" content="hacker-landing.vercel.app" />
+        <meta property="og:site_name" content="HACKER" />
+        <meta property="og:description" content="콩 심은 데 콩 나고, 커밋한 데 머리 난다!" />
+        <meta property="og:image" content="/favicon.png" />
       </Head>
       <Component {...pageProps} />
     </>
