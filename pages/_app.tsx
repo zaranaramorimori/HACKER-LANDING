af8c1acd84757b79d9d81d752046ba822a0af3b2
Danna <daeunlee0720@gmail.com>
2022-05-08 16:11:50 +0900

    feat: 파비콘 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import '@styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
