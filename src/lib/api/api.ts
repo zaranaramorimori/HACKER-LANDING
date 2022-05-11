38da25101673c7c9a8c83c4c5495cfaa7f4ba68b
Danna <daeunlee0720@gmail.com>
2022-05-11 13:33:26 +0900

    fix: 네트워크 타임아웃 시간 연장

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const client = axios.create({
   baseURL: 'https://asia-northeast3-hacker-event.cloudfunctions.net/app/api/',
-  timeout: 1000,
+  timeout: 5000,
 });
